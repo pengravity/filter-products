3f0bab695e4efc919cb38ae8ab3a561ba4c26196
Andy <alipinski@mail.com>
2022-10-17 16:22:11 +0200

    displaying all the products

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
+import { items } from '../../products-data';
 import Categories from '../categories/Categories';
 import Search from '../search/Search';
 import Product from './Product';
 import './ProductList.css';
+import { productsData } from '../../products-data';
 
 const ProductList = () => {
+  const [products, setProducts] = useState(productsData);
+
   return (
     <>
+      {console.log(products)}
       <div className='header'>
         <header className='container'>
           <h1 className='--color-white --text-center'>
@@ -18,7 +25,14 @@ const ProductList = () => {
         </header>
         <div className='product-container'>
           <div className='products container --grid-25 --py2'>
-            <Product />
+            {products.map((product) => {
+              const { id, title, img, price } = product;
+              return (
+                <div key={id}>
+                  <Product title={title} img={img} price={price} />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
